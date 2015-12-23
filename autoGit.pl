463c00a102f09a38318055188a552b1e4c7b77ba
matthew manning <mbhmanning@gmail.com>
2015-12-23 16:06:59 +1300

    ss

---
+++ autoGit.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -w 
+
+#parse user commit message or supply defluat
+$CommitMsg = 'regular commit';#if none supplied
+if(@ARGV)
+{
+	$CommitMsg = '';
+	foreach $Word (@ARGV)
+	{
+		$CommitMsg .= $Word.' ';
+	} 
+	
+}
+$CommitMsg .= "\n";
+
+system('git add *');
+system('git commit -m '.$CommitMsg)
