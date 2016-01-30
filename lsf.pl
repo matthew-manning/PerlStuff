66d24114de199182c7a6443e1e772c9aed11bef2
matthew manning <mbhmanning@gmail.com>
2016-01-30 17:12:40 +1300

    regular commit

--- lsf.pl
+++ lsf.pl
@@ -1,21 +1,59 @@
 #!/usr/bin/perl -w
 use Term::ANSIColor qw(:constants);
+use List::Util "any";##need to update perl to latest version
+
+my $Passed = $ARGV[0]||"";
+#command line args
+
 
 
 
 ###print directory listing
 
-my $DirString = `ls -d $ARGV[0]*/`;
+my $DirString = `ls -d $Passed*/`;
 #^add code to supress warning if use without args
 
-my @CountArr = split /\//,$DirString;
-#^only used to to count number of dirs
-my $NumDirs = scalar(@CountArr)-1;
+my @DirArr = split /$/m, $DirString;
+my $NumDirs = scalar(@DirArr)-1;
 
 print BOLD, BLUE, "\n\n\nDirectories: $NumDirs\n", RESET;
-print BLUE, $DirString, RESET;
+
+
+##
+for(my $i = 0; $i < $NumDirs; $i++)
+{
+	$DirArr[$i] =~ s/$Passed//;
+	$DirArr[$i] =~ s/\///;
+	print BLUE, $DirArr[$i];
+	
+}
+print RESET;
+##code above only works properly for one layer one of ls depth, ie. `ls perl/`
+
+
+
+
 
 ###File listing
 
-my $FileString = `stat -c "%F %n" * | grep "regular file" | cut -d' ' -f 3-`;
-#^figure out how to exclude/remove backup files
+my $FileString = `ls -B $Passed`;
+my @FileArray = split /$/m, $FileString;
+my @FilesToPrints;
+
+
+#push all files(not in dir list) to printing array
+for(my $i = 0; $i < scalar(@FileArray); $i++)
+{
+	if(any(@DirArr) ne $FileArray[$i])
+	{
+		push($FileArr, @FilesToPrint);
+	}
+}
+
+
+my $NumFiles = scalar(@FilesToPrint)-1;
+
+print BOLD, GREEN, "\nFiles and symlinks: $NumFiles\n", RESET;
+print GREEN, "@FilesToPrint\n\n\n", RESET;
+
+###Sym links
