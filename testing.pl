37d02922628947c23e7a0baedf00f1b117848f17
matthew manning <mbhmanning@gmail.com>
2016-02-27 12:51:51 +1300

    file write added to truesight

---
+++ testing.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl -w
+
+$_ = "abaata\n";
+
+s/a/99/g;
+print;
--- truesight.pl
+++ truesight.pl
@@ -7,10 +7,12 @@ unless($ARGV [1])#if no mode flag passed
 	my $Data = `getfattr -n user.truesightdata $File`;
 	
 	$Data =~ s/# file: $File\nuser.truesightdata="//;#removes junk from printed string
-	$Data =~ s/ "\n$//;
+	
 	
 	###need to reaplace \012 with newline
-	$Data =~ s/\\012/\n/; 
+	$Data =~ s/\\012/\n/g;# /g is gobal, replace *ALL* matches  
+	
+	$Data =~ s/"\n$//;#remove more junk
 	
 	
 	print "\n\n$Data\n\n";
@@ -52,11 +54,22 @@ else
 		$PassedStr = $Data.$PassedStr;
 		system("setfattr -n user.truesightdata -v \"$PassedStr\" $File")
 	
-	
 	}
 	elsif($ARGV[1] eq '-f')#replace with named file
 	{
-
+		local $/ = undef;#read entire file with <>
+		open(FILE, '<', "$ARGV[2]")||die "could not open file: $!\n";
+		my $FileData = <FILE>;
+		
+		my $File = $ARGV[0];
+		
+		##
+		print("File data:\n$FileData");
+		##
+		
+		system("setfattr -n user.truesightdata -v \"$FileData\" $File");
+		
+		
 	}
 	elsif($ARGV[1] eq '-fa')#append named file
 	{
